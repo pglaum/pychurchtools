75fa5184453da8957dfc71523be11dce62263f42
Philipp Glaum <philipp.glaum@sinetcon.com>
2024-06-23 18:56:42 +0200

    v0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="churchtools",
-    version="0.2.0",
+    version="0.2.1",
     author="Philipp Glaum",
     author_email="p@pglaum.de",
     description=("A library for the ChurchTools API"),
